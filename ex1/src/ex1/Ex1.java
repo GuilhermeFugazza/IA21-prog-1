bc4d41d4329d015060dd14b72cc096e52b6fdfde
GuilhermeFugazza <104568676+GuilhermeFugazza@users.noreply.github.com>
2022-06-30 15:08:05 -0300

    Add files via upload

---
+++ ex1/src/ex1/Ex1.java
@@ -0,0 +1,19 @@
+package ex1;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        // Teste ex1
+        Bola bolinha = new Bola("azul", "plastico", 45.2);
+        System.out.println("A bola é de cor " + bolinha.mostrarCor());
+        bolinha.trocarCor("verde");
+        System.out.println("A nova cor da bola é " + bolinha.mostrarCor());
+        
+        //Teste Quadrado
+        Quadrado xuxa = new Quadrado(100);
+        System.out.println("O tamanho do quadrado é " + xuxa.mudarValor());
+        xuxa.mudarValor(50);
+        System.out.println("O tamanho do quadrado é " + xuxa.mudarValor());
+    }
+    
+}
