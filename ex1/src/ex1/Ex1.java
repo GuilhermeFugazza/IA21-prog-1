a1f76e1dc9d30d96dd52dca772da9fe7d5956165
GuilhermeFugazza <104568676+GuilhermeFugazza@users.noreply.github.com>
2022-07-07 14:59:37 -0300

    Add files via upload

--- ex1/src/ex1/Ex1.java
+++ ex1/src/ex1/Ex1.java
@@ -11,10 +11,18 @@ public class Ex1 {
         
         //Teste Quadrado
         Quadrado xuxa = new Quadrado(100);
-        System.out.println("O tamanho do quadrado é " + xuxa.retornarValor());
+        System.out.println("\nO tamanho do quadrado é " + xuxa.retornarValor());
         xuxa.mudarValor(50);
         System.out.println("O tamanho do quadrado é " + xuxa.retornarValor());
         xuxa.calcularArea();
         System.out.println("O tamanho do quadrado é " + xuxa.retornarValor());
+        
+       
+        //Teste Pessoa
+        Pessoa linda = new Pessoa("Guilherme", 67.4, 1.85, 20);
+        System.out.println("\nO nome da Pessoa é " + linda.Nome() + "!");
+        System.out.println("E " + linda.Nome() + " está com " + linda.Envelhecer() + " anos de idade");
+        System.out.println("Está com " + linda.Engordar() + "Kg.");
+        System.out.println(linda.Nome() + " Tem " + linda.Crescer() + " metros de altura.");
     }
 }
